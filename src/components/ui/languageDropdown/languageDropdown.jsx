c6568db6141d572987a4c0b1f208f762077b4360
heinminaye <heinminaye8000@gmail.com>
2025-10-22 14:34:59 +0630

    LangguageDropDownfileNameChange

--- src/components/ui/languageDropdown/languageDropdown.jsx
+++ src/components/ui/languageDropdown/languageDropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./LanguageDropdown.css";
 import { useLanguage } from "../../../hooks/useLanguage.js";
 
